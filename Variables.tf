df6f710188b8a5803b0ca16ed7314964f0490eab
dhananjay1611 <ingoledhananjay16@gmail.com>
2025-09-28 16:41:55 +0530

    first commit

---
+++ Variables.tf
@@ -0,0 +1,5 @@
+variable "BucketName" {
+   description = "The name of the S3 bucket"
+   type        = string
+   default     = "dhananjayingole-terraform-bucket"
+ }
---
+++ main.tf
@@ -0,0 +1,71 @@
+resource "aws_s3_bucket" "mys3_bucket" {
+  bucket = var.BucketName
+
+  tags = {
+    Name        = "My-bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = var.BucketName
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = var.BucketName
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = var.BucketName
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "index" {
+  bucket = var.BucketName
+  key    = "index.html"
+  source = "index.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "Error" {
+  bucket = var.BucketName
+  key    = "Error.html"
+  source = "Error.html"
+  acl    = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "profile" {
+  bucket = var.BucketName
+  key    = "profile.png"
+  source = "profile.png"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "website"{
+  bucket = var.BucketName
+
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "Error.html"
+  }
+
+  depends_on = [ aws_s3_bucket_acl.example ]
+}
